1ddcb65464c638f2148f2dd2289174bdc4afc94b
Max Shaptala <maxshaptala@gmail.com>
2020-11-17 11:51:15 +0200

    Перемещено демо из папки stackabuse-platformer в demo

---
+++ module09/demo/game.js
@@ -0,0 +1,30 @@
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 800,
+  heigth: 640,
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 500 },
+      debug: true,
+    },
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() { }
+
+function create() { }
+
+function update() { }
